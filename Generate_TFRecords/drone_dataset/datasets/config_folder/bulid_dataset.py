af5a5e63ea9697df284f74969934f14de031db35
OmerMughal31 <engr.omershahzad@gmail.com>
2020-11-10 13:17:26 +0100

    Update1

---
+++ Generate_TFRecords/drone_dataset/datasets/config_folder/bulid_dataset.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri April  3 13:30:43 2020
+
+@author: Omer
+"""
+
+"""
+This file has been written to generate the training, testing and validation datasets for EffcienctDet.
+
+
+1- Dataset has been labelled with LabelImg.
+2- Annotation files are in XML format
+3- The outputs are train, test, val and classes files which are CSV files generated from XML format annotations.
+
+Usage:
+    1- Please follow the same director structure to optimally use the code.
+        parent:
+            -datasets
+                --config_folder
+                    --config
+                        --paths_config.py
+                    --annotations
+                    --images
+                    --build_dataset.py
+    
+    2- Configuration of base paths and general directory settings
+        i- There is a director named "config" has been created where a PY file names as "paths_config.py" is given.
+        ii- If you are working in docker container, then
+            a- In path-config file change the "BASE_PATH" variable with the aboslute path of parent directory where this file is created. (actually: "datasets/config_folder/dataset")
+        
+        iii- If you are working with spyder of jupyter notebook in python enviornmrnt, then,
+            a- In path-config file change the "BASE_PATH" variable with the annotation director path. (actually: "dataset")
+            
+            #####
+                However this path management is commented in the config file. If you will be using the same directory arrangements, then, you just need to uncomment the path according to working enviornment. Otherwise, please define the base paths carefully.
+                #####
+        
+    3- Place XML annotation files in folder annoptations
+    4- Place images into images folder
+    5- Output CSV files are present in "dataset" directory. NOT IN "datasets" DIRECTORY
+    
+    6- IMPORTANT
+        i- First generate the train.csv and val.csv by setting the --split argument 0.5/0.8/0.9 whatever the user want. It will generate the train and validation set CSV files. Move the train.csv, val.csv and class.csv to 'protected_folder' directory.
+        ***Please put the annotations of for train and test set only in annotation folder at this time.
+        ***Please put the images of for train and test set only in images folder at this time.
+        
+        ii- To generate the test set, please put a none zero (1.0 always) value for --splittest argument vale while keeping the --split argument 0.0. It will generate the permant test set which is not going to change and independent from the concept that howm many times you gnerate your training and validation annotations.
+        ####
+            Replace the empty generated train.csv, val.csv and classes.csv files with CSV files from 'protected_folder'.
+            ####
+            
+        ***Please put the annotations of for only test set in annotation folder at this time.
+        ***Please put the images of for test set only in images folder at this time.
+        
+        ####
+            This is a kind o limitation of program but this strategy has been followed to keep the test set always similar for every model or version of training, while, changing the train or validation set. So that the evaluation of every model is done on the same test set.
+            
+"""
+
+
+# import the necessary packages
+from config import paths_config as config
+from bs4 import BeautifulSoup
+from imutils import paths
+import argparse
+import random
+import os
+
+
+class DataPreparationXml:
+
+    """
+    Functions to define the arguments whcih are always initialted whenever the object of the class is created.
+
+    """
+
+    def __init__(self):
+        # Construct the argument parser and parse the arguments
+        ap = argparse.ArgumentParser()
+        ap.add_argument(
+            "-a", "--annotations", default=config.ANNOT_PATH, help="path to annotations"
+        )
+        ap.add_argument(
+            "-i", "--images", default=config.IMAGES_PATH, help="path to images"
+        )
+        ap.add_argument(
+            "-t",
+            "--train",
+            default=config.TRAIN_CSV,
+            help="path to output training CSV file",
+        )
+        ap.add_argument(
+            "-v",
+            "--val",
+            default=config.VAL_CSV,
+            help="path to output validate CSV file",
+        )
+        ap.add_argument(
+            "-e", "--test", default=config.TEST_CSV, help="path to output test CSV file"
+        )
+        ap.add_argument(
+            "-c",
+            "--classes",
+            default=config.CLASSES_CSV,
+            help="path to output classes CSV file",
+        )
+        ap.add_argument(
+            "-s", "--split", type=float, default=0.0, help="train and val split"
+        )
+        ap.add_argument(
+            "-b", "--splittest", type=float, default=1.0, help="val and test split"
+        )
+        args = vars(ap.parse_args())
+
+        # Create easy variable names for all the arguments
+        # What ever the user will define here,the directory structure should be according to that
+        # Otherwise leave these as these have been defined
+
+        self.annot_path = args["annotations"]
+        self.images_path = args["images"]
+        self.train_csv = args["train"]
+        self.val_csv = args["val"]
+        self.test_csv = args["test"]
+        self.classes_csv = args["classes"]
+        self.train_test_split = args["split"]
+        self.test_val_split = args["splittest"]
+        # initialize the set of classes we have
+        self.CLASSES = set()
+
+    """
+    This Function manages the test train split by shuffeling the paths and applying split afterwords.
+    
+    Inputs:
+        1- Image paths
+        2- Train Val split ratio (already given through arguments. Please change there.)
+        3- Test val split ratio (already given through arguments. Please change there.)
+    
+    Outputs:
+        Managed dataset
+    
+    """
+
+    def test_train_val_splits(
+        self,
+        paths_to_images: str,
+        test_train_split_ratio: float,
+        test_val_split_ratio: str,
+    ) -> list:
+        # grab all image paths then construct the training, validating and testing splits
+        self.image_paths = list(paths.list_files(paths_to_images))
+        random.shuffle(self.image_paths)
+
+        self.division_i = int(len(self.image_paths) * test_train_split_ratio)
+        self.trainimage_paths = self.image_paths[: self.division_i]
+        self.test_val_split_paths = self.image_paths[self.division_i :]
+
+        self.division_j = int(len(self.test_val_split_paths) * test_val_split_ratio)
+        self.testimage_paths = self.test_val_split_paths[: self.division_j]
+        self.valimage_paths = self.test_val_split_paths[self.division_j :]
+
+        # create the list of datasets to build
+        self.dataset = [
+            ("train", self.trainimage_paths, self.train_csv),
+            ("test", self.testimage_paths, self.test_csv),
+            ("val", self.valimage_paths, self.val_csv),
+        ]
+        return self.dataset
+
+    """
+    This function actually writes the train, test and val annotation CSV files
+    
+    Input:
+         Managned data fron the test_train_val_split() function
+    Outputs:
+        None
+    """
+
+    def get_annotations(self, data_to_read: list) -> None:
+        # loop over the datasets
+        for (d_type, image_paths, output_csv) in data_to_read:
+            # load the contents
+            print("[INFO] creating '{}' set...".format(d_type))
+            print("[INFO] {} total images in '{}' set".format(len(image_paths), d_type))
+
+            # open the output CSV file
+            csv = open(output_csv, "w")
+
+            # loop over the image paths
+            for image_path in image_paths:
+                # build the corresponding annotation path
+                fname = image_path.split(os.path.sep)[-1]
+                fname = "{}.xml".format(fname[: fname.rfind(".")])
+                annotPath = os.path.sep.join([self.annot_path, fname])
+
+                # load the contents of the annotation file and buid the soup
+                contents = open(annotPath).read()
+                soup = BeautifulSoup(contents, "html.parser")
+
+                # extract the image dimensions
+                w = int(soup.find("width").string)
+                h = int(soup.find("height").string)
+                # loop over all object elements
+                for o in soup.find_all("object"):
+                    # extract the label and bounding box coordinates
+                    label = o.find("name").string
+                    xmin = int(float(o.find("xmin").string))
+                    ymin = int(float(o.find("ymin").string))
+                    xmax = int(float(o.find("xmax").string))
+                    ymax = int(float(o.find("ymax").string))
+
+                    # truncate any bounding box coordinates that fall outside
+                    # the boundaries of the image
+                    xmin = max(0, xmin)
+                    ymin = max(0, ymin)
+                    xmax = min(w, xmax)
+                    ymax = min(h, ymax)
+
+                    # ignore the bounding boxes where the minimum values are larger
+                    # than the maximum values and vice-versa due to annotation errors
+                    if xmin >= xmax or ymin >= ymax:
+                        continue
+                    elif xmax <= xmin or ymax <= ymin:
+                        continue
+
+                    # write the image path, bb coordinates, label to the output CSV
+                    row = [
+                        os.path.abspath(image_path),
+                        str(xmin),
+                        str(ymin),
+                        str(xmax),
+                        str(ymax),
+                        str(label),
+                    ]
+                    csv.write("{}\n".format(",".join(row)))
+
+                    # update the set of unique class labels
+                    self.CLASSES.add(label)
+
+            # close the CSV file
+            csv.close()
+
+        # write the classes to file
+        print("[INFO] writing classes...")
+        csv = open(self.classes_csv, "w")
+        rows = [",".join([c, str(i)]) for (i, c) in enumerate(self.CLASSES)]
+        csv.write("\n".join(rows))
+        csv.close()
+
+        return None
+
+
+if __name__ == "__main__":
+    dpxml = DataPreparationXml()
+    test_train_val_data = dpxml.test_train_val_splits(
+        dpxml.images_path, dpxml.train_test_split, dpxml.test_val_split
+    )
+    annotations = dpxml.get_annotations(test_train_val_data)
--- README.md
+++ README.md
@@ -1,1 +1,150 @@
-# EfficientDet_modefied
+# EfficientDet
+This is an implementation of [EfficientDet](https://arxiv.org/pdf/1911.09070.pdf) for object detection on Keras and Tensorflow. 
+The project is based on the official implementation [google/automl](https://github.com/google/automl), [fizyr/keras-retinanet](https://github.com/fizyr/keras-retinanet), [qubvel/efficientnet](https://github.com/qubvel/efficientnet), and the [xuannianz/EfficientDet](https://github.com/xuannianz/EfficientDet).
+
+
+## About pretrained weights
+* The pretrained EfficientNet weights on imagenet are downloaded from [Callidior/keras-applications/releases](https://github.com/Callidior/keras-applications/releases)
+* The pretrained EfficientDet weights on coco are converted from the official release [google/automl](https://github.com/google/automl).
+
+Thanks for their hard work.
+This project is released under the Apache License. Please take their licenses into consideration too when use this project.
+
+
+# Usage
+**Usage within Docker Container**
+* Docker file is developed inside this repository along with .devcontainer folder. Please build the image and run inside bash environments.txt
+
+
+**Usage in Python Environment**
+* Create Python environment
+* Then, to install the dependencies in created environments
+```
+pip install requirement
+```
+## Training
+
+### Training for benchmark Datasets
+
+**Build dataset** 
+1. Pascal VOC 
+    * Download VOC2007 and VOC2012, copy all image files from VOC2007 to VOC2012.
+    * Append VOC2007 train.txt to VOC2012 trainval.txt.
+    * Overwrite VOC2012 val.txt by VOC2007 val.txt.
+2. MSCOCO 2017
+    * Download images and annotations of coco 2017
+    * Copy all images into datasets/coco/images, all annotations into datasets/coco/annotations
+
+### Training for custom datasets
+
+**Build dataset**
+
+* I have used LableImg to label my dataset, therefore, the annotations are in XML format and will be converted into CSV format.
+  * If you have not written your own script for XML -> CSV, then, a setup with different PY files is given in director (datasets/config_folder/..). 
+    1. Python file "build_dataset.py" is the required script for XML -> CSV conversion.
+    2. Place your annotations in annotation folder
+    3. Place your images in images folder
+    4. Follow the instruction written in "build_dataset.py" file to configure "config/paths_config.py" file for correct functionality either in Docker container or Python environment.
+    5. Outputs of these scripts will be
+        1. train.csv, val.csv, test.csv and classes.csv
+  * Write your own script to generate CSV files of annotations with format
+    1. Annotations format
+       1. The CSV file with annotations should contain one annotation per line.
+       2. Images with multiple bounding boxes should use one row per bounding box.
+       3. Note that indexing for pixel values starts at 0.
+       4. The expected format of each line is
+
+       ```
+       path/to/image.jpg,x1,y1,x2,y2,class_name
+       ```
+
+        5. Some images may not contain any labeled objects. To add these images to the dataset as negative examples, add an annotation where `x1`, `y1`, `x2`, `y2` and `class_name` are all empty:
+
+        ``` 
+        path/to/image.jpg,,,,, 
+        ```
+
+        A full example:
+
+        ```
+        /data/imgs/img_001.jpg,837,346,981,456,cow
+        /data/imgs/img_002.jpg,215,312,279,391,cat
+        /data/imgs/img_002.jpg,22,5,89,84,bird
+        /data/imgs/img_003.jpg,,,,,
+        ```
+        
+        6. This defines a dataset with 3 images.
+        `img_001.jpg` contains a cow.
+        `img_002.jpg` contains a cat and a bird.
+        `img_003.jpg` contains no interesting objects/animals.
+
+
+    2. Class mapping format:
+        The class name to ID mapping file should contain one mapping per line.
+        Each line should use the following format:
+
+        ```
+        class_name,id
+        ```
+        
+        Indexing for classes starts at 0.
+        Do not include a background class as it is implicit.
+
+        For example:
+
+        ```
+        cow,0
+        cat,1
+        bird,2
+        ```
+
+**train**
+
+```
+python train.py --phi 1 --gpu 0 --random-transform --compute-val-loss --batch-size 3 --steps 1000 --epochs 500 csv path_to_train_files/train.csv path_to_classes_file/classes.csv --val-annotations-path path_to_val_dataset_file/val.csv
+```
+
+ 1. If my dataset arrangement format has been followed and CSV fils has been generated with "build_dataset.py" then, please use the following command.
+    ```
+    python train.py --phi 1 --gpu 0 --random-transform --compute-val-loss --batch-size 3 --steps 1000 --epochs 500 csv datasets/config_folder/dataset/train.csv datasets/config_folder/dataset/classes.csv --val-annotations-path datasets/config_folder/dataset/val.csv
+    ```
+
+**Anchors Optimization**
+
+Sometimes, the default anchor boxes are not suitable, therefore, the small objects can not be detected. To get rid of this problem, the anchor boxes can be optimized.
+* Either use the anchor optimizations repository cloned in this implementation as "anchor-optimization-master" or follow the [anchor-optimization](https://github.com/martinzlocha/anchor-optimization/).
+
+## Evaluate
+1. PASCAL VOC
+    * `python3 eval/common.py` to evaluate pascal model by specifying model path there.
+    * The best evaluation results (score_threshold=0.01, mAP<sub>50</sub>) on VOC2007 test are: 
+
+    | phi | 0 | 1 |
+    | ---- | ---- | ---- |
+    | w/o weighted |  | [0.8029](https://drive.google.com/open?id=1-QkMq56w4dZOTQUnbitF53NKEiNF9F_Q) |
+    | w/ weighted | [0.7892](https://drive.google.com/open?id=1mrqL9rFoYW-4Jc57MsTipkvOTRy_EGfe) |  |
+2. MSCOCO
+    * `python3 eval/coco.py` to evaluate coco model by specifying model path there.
+    
+    | phi | mAP |
+    | ---- | ---- |
+    | 0 | 0.334 [weights](https://drive.google.com/open?id=1MNB5q6rJ4TK_gen3iriu8-ArG9jB8aR9), [results](https://drive.google.com/open?id=1U4Bdk4C7aNF7l4mvhh2Oi8mFpttEwB8s) |
+    | 1 | 0.393 [weights](https://drive.google.com/open?id=11pQznCTi4MaVXqkJmCMcQhphMXurpx5Z), [results](https://drive.google.com/open?id=1NjGr3yG3_Rk1xVCk4sgVelTZNNz_E2vp) |
+    | 2 | 0.424 [weights](https://drive.google.com/open?id=1_yXrOrY0FDnH-d_FQIPbGy4z2ax4aNh8), [results](https://drive.google.com/open?id=1UQP8kDj7tXHC2bs--Aq8x7w7FkVX4xJD) |
+    | 3 | 0.454 [weights](https://drive.google.com/open?id=1VnxoBpEQmm0Z2uO3gjhYDeu-rNirba6c), [results](https://drive.google.com/open?id=1uruTEMPhl_JvbA_T9kCdutzeOR3gFX4g) |
+    | 4 | 0.483 [weights](https://drive.google.com/open?id=1lQvTpnO_mfkHCRpcP28dxU4CWyK3xUzj), [results](https://drive.google.com/open?id=1s4nmgYaPqjbAgDlRF1AVVz6uWKDz7O_i) |
+
+3. Custom Data (Testset)
+    * Prepare the "test.csv" file
+    * Arrange test images in the directory according to CSV annotation so that these could be accessible by "eveluate_model_mAP.py" file.
+    * If you have used "build_dataset.py" file to generate all of your CSV files, then, "eveluate_model_mAP.py" has already been configured with directory paths.
+    * Please specify the model path to use the weight matrix and model
+    * Specify the corresponding "phi" value. For instance, 0 for EfficientDet D0, 1 for EfficientDet D1 etc.
+## Test 
+
+* Prepare the test image files in a folder
+* "eveluate_model_image_output.py" has already been configured with directory paths.
+* Please specify the model path to use the weight matrix and model
+* Specify the corresponding "phi" value. For instance, 0 for EfficientDet D0, 1 for EfficientDet D1 etc.
+
+[//]: ![image1](test/demo.jpg) 
